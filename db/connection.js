79e836c438565606d80de511e7ec4307da0375e1
Olha Dudchenko <dudchenko.olha92@gmail.com>
2021-11-22 13:05:51 +0200

    change path

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const MONGO_URL = process.env.MONGO_URL
+
+// TODO: put a .env file istead of .env.example in project to read MONGO_URL
+const MONGO_URL = process.env.DB_HOST
 
 async function main() {
   await mongoose.connect(MONGO_URL)
