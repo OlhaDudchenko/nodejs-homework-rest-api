7cf4655e54077fa707caf6fab4ebdd40c3b221cd
Olha Dudchenko <dudchenko.olha92@gmail.com>
2021-12-08 14:37:36 +0200

    remove functions controllers

--- controllers/auth/current.js
+++
@@ -1,9 +0,0 @@
-const { currentUser } = require('../../services/auth')
-
-const currentUserController = async (req, res, next) => {
-  const { _id } = req.user
-  const user = await currentUser({ _id })
-  res.status(200).json(user)
-}
-
-module.exports = currentUserController
